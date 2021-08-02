d8809447c2ac5d9d5483baa9a3d301bb226fc759
lizajolina <lizajolina@gmail.com>
2021-08-02 20:19:37 +1000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { camerakit } from 'camerakit';
 
 // 2. Add your credentials from step 1
 const config = {
@@ -30,40 +29,5 @@ const config = {
 })
 export class AppModule { }
 
-async function () {
-  const devices = await camerakit.getDevices();
- 
-  const preview = await camerakit.createCaptureStream({
-    audio: devices.audio[0],
-    video: devices.video[0]
-  });
- 
-  preview.setResolution({width: 1920, height: 1080});
-  const myPicture = preview.shutter.capture();
- 
-  preview.recorder.start();
- 
-  // Wait...
- 
-  // Pause the recording & resume
-  await preview.recorder.pause();
-  await preview.recorder.start();
- 
-  // Wait some more...
- 
-  const recordedVideo = await preview.recorder.stop(); // Use the video yourself
- 
-  preview.recorder.downloadLatestRecording(); // Download the video direct from browser
- 
-  // Stop using camera
-  preview.destroy();
- 
-  // Play video via camerakit player
-  const player = new camerakit.Player();
-  player.src = window.URL.createObjectURL(recordedVideo);
- 
-  // Use the video player wherever you want!
-  document.getElementById("your-player-container").appendChild(player);
-  player.play();
-}
+
 
